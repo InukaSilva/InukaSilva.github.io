2a8c45712c4c484089ae9d4dd05c2b4181bcd252
Inuka <InukaSilva@outlook.com>
2023-11-24 17:03:36 -0500

    remove_galler/

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,9 +1,9 @@
-const imageList = [{ src: "gallery/art/Image_1.jpg", alt: "image" }, 
-{ src: "gallery/art/Image_2", alt: "image" }, 
-{ src: "gallery/art/Image_3", alt: "image" }, 
-{ src: "gallery/art/Image_4", alt: "image" }, 
-{ src: "gallery/art/Image_5", alt: "image" }, 
-{ src: "gallery/art/Image_6", alt: "image" },];
+const imageList = [{ src: "art/Image_1.jpg", alt: "image" }, 
+{ src: "art/Image_2", alt: "image" }, 
+{ src: "art/Image_3", alt: "image" }, 
+{ src: "art/Image_4", alt: "image" }, 
+{ src: "art/Image_5", alt: "image" }, 
+{ src: "art/Image_6", alt: "image" },];
 
 const row = document.querySelector(".row");
 for (let i = 0; i < 3; i++) {
